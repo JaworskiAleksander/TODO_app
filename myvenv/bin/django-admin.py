349461bc8adc6ddbff3d416ed1b7a894df266fc7
Aleksander Jaworski <aleck156@gmail.com>
2020-05-03 09:29:30 +0200

    updated pip, installed django, updated requirements.txt

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/railgun/Pulpit/Full Stack Web Dev/Python/Django_Code_Deployment/TODO_app/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
