4f04950e50b63bf7629294cfd2abb38cdffa789a
Aleksander Jaworski <aleck156@gmail.com>
2020-05-04 09:37:06 +0200

    show if user is logged in

--- TODO_app/settings.py
+++ TODO_app/settings.py
@@ -14,8 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TODO_app',
     'TODO',
 ]
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TODO_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'TODO/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
