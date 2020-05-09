98b064a984f43e991567d7bc7c44c0762e4d8c2a
Aleksander Jaworski <aleck156@gmail.com>
2020-05-09 13:47:41 +0200

    test

--- TODO_app/settings.py
+++ TODO_app/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
+for item in os.listdir(STATIC_ROOT):
+    print(item)
 STATIC_URL = '/static/'
 
 LOGIN_URL = 'login/'
