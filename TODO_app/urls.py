c5e49f93b43365f1bd023ac980249ec88d93b4a1
Aleksander Jaworski <aleck156@gmail.com>
2020-05-12 12:12:48 +0200

    added static

--- TODO_app/urls.py
+++ TODO_app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TODO import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,4 +37,4 @@ urlpatterns = [
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
